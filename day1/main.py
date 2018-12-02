3803e579f0bfecf2711b0e955581c7cf910d38b4
Yohann Bacha <8.bachrc@gmail.com>
2018-12-02 15:55:41 +0100

    Solved Day 1 part two

--- day1/main.py
+++ day1/main.py
@@ -8,6 +8,22 @@ if __name__ == '__main__':
     final_frequency = 0
 
     for line in lines:
-        final_frequency += int(line)
+        frequency = int(line)
+        final_frequency += frequency
 
-    print(final_frequency)
+    total_looped_frequency = 0
+    frequencies = []
+    i = 0
+
+    while True:
+        if total_looped_frequency in frequencies:
+            break
+
+        frequency = int(lines[i])
+
+        frequencies.append(total_looped_frequency)
+        total_looped_frequency += frequency
+        i = (i + 1) % len(lines)
+
+    print("The final frequency is : {}\nThe first redundant frequency is {}".format(final_frequency,
+                                                                                    total_looped_frequency))
